f36d767bab9b4a8b1ce7a95becd771efa649d239
Lennard <lk@oeko-planet.de>
2023-12-07 09:55:22 +0100

    fix

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ window.addEventListener('scroll', function() {
     var scrollPosition = window.scrollY || document.documentElement.scrollTop;
     var containerHeight = document.querySelector('.img-container').offsetHeight;
     var percentage = scrollPosition / window.innerHeight;
-    var visibleHeight = Math.min(containerHeight, percentage * 2 * containerHeight);
+    var visibleHeight = Math.min(containerHeight, percentage * 3 * containerHeight);
 
     document.querySelector('.lk-comic').style.clip = `rect(auto, auto, ${visibleHeight}px, auto)`;
 });
