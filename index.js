2d2470b34d53ecbe7b8b083b88a576140cce0ea3
Lennard <lk@oeko-planet.de>
2023-12-07 10:10:53 +0100

    changed from clip to clip path because deprecated

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-window.addEventListener('scroll', function() {
+window.addEventListener('scroll', function () {
     var scrollPosition = window.scrollY || document.documentElement.scrollTop;
     var containerHeight = document.querySelector('.img-container').offsetHeight;
     var percentage = scrollPosition / window.innerHeight;
     var visibleHeight = Math.min(containerHeight, percentage * 3 * containerHeight);
 
-    document.querySelector('.lk-comic').style.clip = `rect(auto, auto, ${visibleHeight}px, auto)`;
+    var clipValue = (containerHeight - visibleHeight) / containerHeight * 100;
+    
+    document.querySelector('.lk-comic').style.clipPath = `inset(${clipValue}% 0 0 0)`;
 });
