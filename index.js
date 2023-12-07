ff1b32fb7bf910d7ca834ae8a3193ff455ef8dec
Lennard <lk@oeko-planet.de>
2023-12-07 16:31:25 +0100

    some design improvements

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
-window.addEventListener('scroll', function () {
-    var scrollPosition = window.scrollY || document.documentElement.scrollTop;
-    var containerHeight = document.querySelector('.img-container').offsetHeight;
-    var percentage = scrollPosition / window.innerHeight;
-    var visibleHeight = Math.min(containerHeight, percentage * 3 * containerHeight);
-
-    var clipValue = (containerHeight - visibleHeight) / containerHeight * 100;
-    
-    document.querySelector('.lk-comic').style.clipPath = `inset(${clipValue}% 0 0 0)`;
-});
-
 var elementToScroll = document.querySelector('.scrolling-element');
-var stopPosition = 250;
+var stopPosition = 500;
 
 window.addEventListener('scroll', function() {
-    var scrolled = window.scrollY;
+    var scrolled = window.scrollY || document.documentElement.scrollTop;
 
     if (scrolled < stopPosition) {
         elementToScroll.style.transform = 'translateY(' + scrolled + 'px)';
     }
+
+    var containerHeight = document.querySelector('.img-container').offsetHeight;
+    var percentage = scrolled / window.innerHeight;
+    var visibleHeight = Math.min(containerHeight, percentage * 1.3 * containerHeight);
+
+    var clipValue = (containerHeight - visibleHeight) / containerHeight * 100;
+    
+    document.querySelector('.lk-comic').style.clipPath = `inset(${clipValue}% 0 0 0)`;
 });
