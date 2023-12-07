047aea00676de1f3a9aac16b7f877b70e8c250b5
Lennard <lk@oeko-planet.de>
2023-12-07 09:21:45 +0100

    little bit longer animation

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ window.addEventListener('scroll', function() {
     var scrollPosition = window.scrollY || document.documentElement.scrollTop;
     var comicImage = document.querySelector('.lk-comic');
 
-    if (scrollPosition > 200) {
+    if (scrollPosition > 100) {
         comicImage.classList.add('show-comic');
     } else {
         comicImage.classList.remove('show-comic');
