57476c9d32a4925adcf02c4b70bc600b6d5adc6c
Lennard Kießling <lennard-2@web.de>
2023-12-08 21:31:45 +0100

    steigung für responsive berechnet

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ var elementToScroll = document.querySelector('.scrolling-element');
 var lkcomic = document.querySelector('.lk-comic');
 var lknormal = document.querySelector('.lk-normal');
 var elementPart2 = document.querySelector('.part-2');
+var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+var Loesung = (0.347 * width) + 127.6;
+
+
+
 
 
 function avatarPositionScroll() {
@@ -12,14 +17,15 @@ function avatarPositionScroll() {
         elementToScroll.style.transform = 'translateY(' + scrolled + 'px)';
     }
 
-    var clipValue = ((stopPosition - scrolled * 1.3) / stopPosition * 100) + 8;
-    var clipValue1 = 92 - ((stopPosition - scrolled * 1.3) / stopPosition * 100);
-    console.log(clipValue1)
-    if (20 < clipValue1 <= 100 && 80 > clipValue >= 0){
-        lkcomic.style.clipPath = `inset(${clipValue}% 0 0 0)`;
-        lknormal.style.clipPath = `inset( 0 0 ${clipValue1}% 0)`;
-    }
+    var clipValue = (stopPosition - scrolled) - 138;
+    var clipValue1 = Loesung - (stopPosition - scrolled);
+    lkcomic.style.clipPath = `inset(${clipValue}px 0 0 0)`;
+    lknormal.style.clipPath = `inset( 0 0 ${clipValue1}px 0)`;
+
+
 }
 
+
 document.addEventListener('scroll', avatarPositionScroll);
-document.addEventListener('touchmove', avatarPositionScroll);
+document.addEventListener('click', setTimeout(avatarPositionScroll, 1))
+document.onload(avatarPositionScroll());
