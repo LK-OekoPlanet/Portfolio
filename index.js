da23adbc6c067f87063d37a53d14fbe672bf2671
Lennard <lk@oeko-planet.de>
2023-12-08 14:25:23 +0100

    some testing for responsive stopping

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var elementToScroll = document.querySelector('.scrolling-element');
 var lkcomic = document.querySelector('.lk-comic');
 var lknormal = document.querySelector('.lk-normal');
-var stopPosition = 500;
+var elementPart2 = document.querySelector('.part-2')
+
 
 function avatarPositionScroll() {
     var scrolled = window.scrollY || document.documentElement.scrollTop;
 
+    var stopPosition = elementPart2.offsetTop; // Die Y-Position von elementPart2 relativ zum Anfang des Dokuments
     if (scrolled < stopPosition) {
         elementToScroll.style.transform = 'translateY(' + scrolled + 'px)';
     }
@@ -16,7 +18,7 @@ function avatarPositionScroll() {
 
     var clipValue = (containerHeight - visibleHeight) / containerHeight * 100;
     var clipValue1 = 100 - (containerHeight - visibleHeight) / containerHeight * 100;
-    
+
     lkcomic.style.clipPath = `inset(${clipValue}% 0 0 0)`;
     lknormal.style.clipPath = `inset( 0 0 ${clipValue1}% 0)`;
 
