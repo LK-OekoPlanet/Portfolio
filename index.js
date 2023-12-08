eb6acc1301561f6fbe638f2d3ff77ce57aeed9dd
Lennard <lk@oeko-planet.de>
2023-12-08 13:45:21 +0100

    comic picture me (might change bc too formal)

--- index.js
+++ index.js
@@ -10,9 +10,12 @@ window.addEventListener('scroll', function() {
 
     var containerHeight = document.querySelector('.img-container').offsetHeight;
     var percentage = scrolled / window.innerHeight;
-    var visibleHeight = Math.min(containerHeight, percentage * 1.3 * containerHeight);
+    var visibleHeight = Math.min(containerHeight, percentage * 1.5 * containerHeight);
 
     var clipValue = (containerHeight - visibleHeight) / containerHeight * 100;
+    var clipValue1 = 100 - (containerHeight - visibleHeight) / containerHeight * 100;
     
     document.querySelector('.lk-comic').style.clipPath = `inset(${clipValue}% 0 0 0)`;
+    document.querySelector('.lk-normal').style.clipPath = `inset( 0 0 ${clipValue1}% 0)`;
+
 });
