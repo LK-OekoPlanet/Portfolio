6f8d0ecfb6b01439e0ec0a37f63d37743559451b
Lennard Kießling <lennard-2@web.de>
2023-12-08 23:41:44 +0100

    added some functionality and fixed some bugs

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ var elementToScroll = document.querySelector('.scrolling-element');
 var lkcomic = document.querySelector('.lk-comic');
 var lknormal = document.querySelector('.lk-normal');
 var elementPart2 = document.querySelector('.part-2');
-var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
-var Loesung = (0.347 * width) + 127.6;
-
-
+var Loesung;
+var width;
+function initilizeVariables() {
+    width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+    // Steigungsformel um die Responsiveness des richtigen Umbruchs zu bestimmen
+    Loesung = (0.347 * width) + 127.6;
+}
 
 
 
@@ -17,15 +20,22 @@ function avatarPositionScroll() {
         elementToScroll.style.transform = 'translateY(' + scrolled + 'px)';
     }
 
+    // 138 stimmt immer als Wert
     var clipValue = (stopPosition - scrolled) - 138;
+    // Loesung wird berechnet
     var clipValue1 = Loesung - (stopPosition - scrolled);
-    lkcomic.style.clipPath = `inset(${clipValue}px 0 0 0)`;
-    lknormal.style.clipPath = `inset( 0 0 ${clipValue1}px 0)`;
 
+    if (clipValue > 0 && 0 < clipValue1 < 1000){
+        lkcomic.style.clipPath = `inset(${clipValue}px 0 0 0)`;
+        lknormal.style.clipPath = `inset( 0 0 ${clipValue1}px 0)`;
+    }
 
 }
 
-
+document.addEventListener('DOMContentLoaded',initilizeVariables);
+document.addEventListener('DOMContentLoaded',avatarPositionScroll);
 document.addEventListener('scroll', avatarPositionScroll);
-document.addEventListener('click', setTimeout(avatarPositionScroll, 1))
-document.onload(avatarPositionScroll());
+window.addEventListener('resize', initilizeVariables);
+window.addEventListener('resize', avatarPositionScroll);
+
+
